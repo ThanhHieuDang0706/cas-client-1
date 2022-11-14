722ca5843b441ad1a39d06dc5b81fba7b31525cb
Thanh Hieu <hieu.dang.dth_is_me@hcmut.edu.vn>
2022-11-14 10:38:50 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,159 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useContext, useEffect, useState } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Button, Box, Text, Paragraph, Spinner, Heading } from 'grommet';
+import { Login, Logout } from 'grommet-icons';
+import { useHistory } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { CasUserContext, useCas } from './cas/index';
+
+export default function App() {
+	return (
+		<Router>
+			<Switch>
+				<Route exact path='/home'>
+					<SecureHome />
+				</Route>
+				<Route exact path='/'>
+					<Home />
+				</Route>
+				<Route>
+					<NoMatch />
+				</Route>
+			</Switch>
+		</Router>
+	);
 }
 
-export default App;
+function Layout(props) {
+	const history = useHistory();
+	const [securityChecked, setSecurityChecked] = useState(false);
+	const casUserContext = useContext(CasUserContext);
+
+	useEffect(() => {
+		if (props.isSecure && !casUserContext?.user) {
+			history.replace('/');
+		} else {
+			setSecurityChecked(true);
+		}
+	}, [props.isSecure, casUserContext?.user]);
+
+	if (!securityChecked) return <Box></Box>;
+
+	return (
+		<Box align='center' background={props.background} fill>
+			<Heading
+				margin={{
+					top: 'xlarge',
+					bottom: 'none',
+				}}
+			>
+				CAS Client 1
+			</Heading>
+
+			<Box justify='center' align='center'>
+				{props.children}
+			</Box>
+		</Box>
+	);
+}
+
+function Home() {
+	const history = useHistory();
+	const cas = useCas(true);
+	const casUserContext = useContext(CasUserContext);
+
+	useEffect(() => {
+		if (casUserContext?.user) {
+			history.replace('/home');
+		}
+	}, [casUserContext?.user]);
+
+	return (
+		<Layout background='status-unknown' isSecure={false}>
+			{cas.isLoading && (
+				<Box align='center' gap='medium'>
+					<Spinner
+						border={[
+							{
+								side: 'all',
+								color: 'brand',
+								size: 'medium',
+								style: 'dotted',
+							},
+						]}
+					/>
+					<Text>Redirecting ...</Text>
+				</Box>
+			)}
+			{!cas.isLoading && (
+				<Box align='center' gap='xsmall'>
+					<Paragraph textAlign='center'>
+						Hello anonymous! Please click{' '}
+						<Text color='brand' size='large'>
+							LOGIN
+						</Text>{' '}
+						with follows account.
+					</Paragraph>
+					
+					<Button
+						label='Login'
+						icon={<Login />}
+						a11yTitle='Login button'
+						margin='medium'
+						reverse
+						onClick={() => {
+							cas.attemptCasLogin(false);
+						}}
+					/>
+				</Box>
+			)}
+		</Layout>
+	);
+}
+
+function SecureHome() {
+	const cas = useCas();
+	const casUserContext = useContext(CasUserContext);
+
+	return (
+		<Layout background='status-ok' isSecure={true}>
+			<Box align='center' gap='medium'>
+				<Paragraph textAlign='center'>
+					welome{' '}
+					<Text color='brand' size='xxlarge'>
+						{casUserContext?.user}
+					</Text>
+				</Paragraph>
+			</Box>
+			<Button
+				label='Logout'
+				icon={<Logout />}
+				a11yTitle='Logout button'
+				reverse
+				onClick={() => {
+					cas.logout();
+				}}
+			/>
+		</Layout>
+	);
+}
+
+function NoMatch() {
+	const history = useHistory();
+
+	return (
+		<Layout background='status-critical' isSecure={false}>
+			<Box align='center' gap='medium'>
+				<Paragraph textAlign='center'>Page Not Found.</Paragraph>
+				<Button
+					label='Go Back'
+					color='white'
+					a11yTitle='Go Back button'
+					onClick={() => {
+						history.replace('/');
+					}}
+				/>
+			</Box>
+		</Layout>
+	);
+}
---
+++ src/cas/index.jsx
@@ -0,0 +1,2 @@
+export { default as useCas } from './useCas';
+export * from './casUserContext';
